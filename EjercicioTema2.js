9edc848c01e2bf490ed1a62b7981a9605e62e5d1
Aitor Reviriego Amor <45858915+aitorevi@users.noreply.github.com>
2022-10-05 23:39:08 +0200

    Ejercicio tema 2

---
+++ EjercicioTema2.js
@@ -0,0 +1,12 @@
+let lista = {
+  nombre: "Aitor",
+  edad: 3,
+  "eres desarrollador": true,
+  fNacimiento: Date("september 17 1979"),
+  libro: {
+    titulo: "Abitos atómicos",
+    autor: "James Clear",
+    fecha: Date("september 8 2020"),
+    url: "https://www.amazon.es/H%C3%A1bitos-at%C3%B3micos-Autoconocimiento-James-Clear/dp/8418118032",
+  },
+};
